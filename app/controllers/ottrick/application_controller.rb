176974e88183b80810b0c205d78bd7d50008d5ca
Wolfgang Barth <wob@swobspace.net>
2014-06-03 19:32:11 +0200

    wobapphelpers adaptions to application_controller

--- app/controllers/ottrick/application_controller.rb
+++ app/controllers/ottrick/application_controller.rb
@@ -1,4 +1,15 @@
 module Ottrick
   class ApplicationController < ActionController::Base
+    # -- use the application default layout
+    layout "application"
+
+    # -- breadcrumbs
+    include Wobapphelpers::Breadcrumbs
+    before_filter :add_breadcrumb_index, only: [:index]
+
+    # -- flash responder
+    self.responder = Wobapphelpers::Responders
+    respond_to :html, :json
+
   end
 end

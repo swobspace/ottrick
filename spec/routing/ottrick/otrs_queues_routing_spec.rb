c690e92e057fafe512adb96d891be22c49e7787c
Wolfgang Barth <wob@swobspace.net>
2014-06-04 20:14:40 +0200

    fix routing specs

--- spec/routing/ottrick/otrs_queues_routing_spec.rb
+++ spec/routing/ottrick/otrs_queues_routing_spec.rb
@@ -2,34 +2,36 @@ require "rails_helper"
 
 module Ottrick
   RSpec.describe OtrsQueuesController, :type => :routing do
+    routes { Ottrick::Engine.routes }
+
     describe "routing" do
 
       it "routes to #index" do
-        expect(:get => "/otrs_queues").to route_to("otrs_queues#index")
+        expect(:get => "/otrs_queues").to route_to("ottrick/otrs_queues#index")
       end
 
       it "routes to #new" do
-        expect(:get => "/otrs_queues/new").to route_to("otrs_queues#new")
+        expect(:get => "/otrs_queues/new").to route_to("ottrick/otrs_queues#new")
       end
 
       it "routes to #show" do
-        expect(:get => "/otrs_queues/1").to route_to("otrs_queues#show", :id => "1")
+        expect(:get => "/otrs_queues/1").to route_to("ottrick/otrs_queues#show", :id => "1")
       end
 
       it "routes to #edit" do
-        expect(:get => "/otrs_queues/1/edit").to route_to("otrs_queues#edit", :id => "1")
+        expect(:get => "/otrs_queues/1/edit").to route_to("ottrick/otrs_queues#edit", :id => "1")
       end
 
       it "routes to #create" do
-        expect(:post => "/otrs_queues").to route_to("otrs_queues#create")
+        expect(:post => "/otrs_queues").to route_to("ottrick/otrs_queues#create")
       end
 
       it "routes to #update" do
-        expect(:put => "/otrs_queues/1").to route_to("otrs_queues#update", :id => "1")
+        expect(:put => "/otrs_queues/1").to route_to("ottrick/otrs_queues#update", :id => "1")
       end
 
       it "routes to #destroy" do
-        expect(:delete => "/otrs_queues/1").to route_to("otrs_queues#destroy", :id => "1")
+        expect(:delete => "/otrs_queues/1").to route_to("ottrick/otrs_queues#destroy", :id => "1")
       end
 
     end

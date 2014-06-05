3826dc95a0aaf785927400a84be272660441b68b
Wolfgang Barth <wob@swobspace.net>
2014-06-05 19:09:33 +0200

    validations for otrs_queue

--- app/models/ottrick/otrs_queue.rb
+++ app/models/ottrick/otrs_queue.rb
@@ -1,4 +1,12 @@
 module Ottrick
   class OtrsQueue < ActiveRecord::Base
+    # -- associations
+    # -- configuration
+    # -- validations and callbacks
+    validates :name, :presence => true, :uniqueness => true
+
+    def to_s
+      name
+    end
   end
 end

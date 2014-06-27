033fc28ac0a86962742469e47a53e106e90e6630
Wolfgang Barth <wob@swobspace.net>
2014-06-27 19:01:00 +0200

    delegate ottick module accessors from ottrick

--- lib/ottrick.rb
+++ lib/ottrick.rb
@@ -8,7 +8,34 @@ module Ottrick
 
   # ticketfor_types
   #
-  mattr_reader :ticketfor_types
+  mattr_accessor :ticketfor_types
   @@ticketfor_types = [ ]
 
+
+  # delegate to Ottick
+  OTTICK_ACCESSORS = [
+    "endpoint",		# endpoint for soap
+    "wsdl",		# i.e. GenericTicketConnector.wsdl
+    "http_auth_user",	# basic auth user if OTRS is configured with http_authentication
+    "http_auth_passwd",	# 
+    "otrs_user",	# valid OTRS user
+    "otrs_passwd",	# 
+    "ticket_queue",	# ticket queue used as default
+    "ticket_priority",	# ticket priority used as default
+    "ticket_state",	# ticket state used as default
+    "ticket_type",	# ticket type used as default
+    "customer_user",	# sender email address used as default
+    "article_sender_type", # defaults ...
+    "article_charset",	   # 
+    "article_mime_type",   # 
+  ]
+   
+  OTTICK_ACCESSORS.each do |m|
+    define_singleton_method m.to_sym do
+      Ottick.send(m)
+    end
+    define_singleton_method (m + '=').to_sym do |param|
+      Ottick.send(m + '=', param)
+    end
+  end
 end
--- spec/ottrick_spec.rb
+++ spec/ottrick_spec.rb
@@ -4,5 +4,22 @@ describe 'Ottrick engine' do
   it "Ottrick module is available" do
     expect(Ottrick).to be_a_kind_of Module
   end
+
+  it "delegates endpoint and endpoint= to Ottick" do
+    Ottrick.endpoint = "example"
+    expect(Ottick.endpoint).to be == "example"
+    Ottick.endpoint = "reverse back"
+    expect(Ottrick.endpoint).to be == "reverse back"
+  end
+
+  Ottrick::OTTICK_ACCESSORS.each do |accessor|
+    it "delegates #{accessor} to Ottick" do
+      Ottrick.send(accessor + '=',  "set value")
+      expect(Ottick.send(accessor)).to be == "set value"
+
+      Ottick.send(accessor + '=', "read value back")
+      expect(Ottrick.send(accessor)).to be == "read value back"
+    end
+  end
 end
 

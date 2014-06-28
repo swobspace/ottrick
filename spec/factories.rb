fb240bfff5ac26db62e41ab27017133a987e8702
Wolfgang Barth <wob@swobspace.net>
2014-06-28 20:17:55 +0200

    skip after_create callback for ticket factory
      see http://stackoverflow.com/questions/8751175/skip-callbacks-on-factory-girl-and-rspec
      for further informations

--- spec/factories.rb
+++ spec/factories.rb
@@ -13,6 +13,16 @@ FactoryGirl.define do
     text    "should be a concrete description"
     sender  "tester@example.com"
     otrs_queue_id 1
+    # -- FactoryGirl.create(:ticket)
+    after(:build) do |ticket| 
+      ticket.class.skip_callback(:create, :after, :create_otrs_ticket)
+    end
+    # -- FactoryGirl.create(:ticket, :with_after_save_callback)
+    trait :with_after_save_callback do
+      after(:build) do |ticket| 
+        ticket.class.set_callback(:create, :after, :create_otrs_ticket)
+      end
+    end
   end
 
 

fc98f847a556a0d6742f8168754ced44c795f616
Wolfgang Barth <wob@swobspace.net>
2014-06-29 18:43:15 +0200

    otrs_ticket_link helper implemented

--- app/helpers/ottrick/tickets_helper.rb
+++ app/helpers/ottrick/tickets_helper.rb
@@ -1,4 +1,17 @@
 module Ottrick
   module TicketsHelper
+    def otrs_ticket_link(ticket, variant)
+      if variant == :id
+        link_to ticket.otrs_ticket_id, 
+          "#{Ottrick.ticket_zoom_url};TicketID=#{ticket.otrs_ticket_id}",
+	  target: :blank
+      elsif variant == :number
+        link_to ticket.ticketnumber, 
+          "#{Ottrick.ticket_zoom_url};TicketNumber=#{ticket.ticketnumber}",
+	  target: :blank
+      else
+        raise RuntimeError, "ticket link variant #{variant} not yet implemented"
+      end
+    end
   end
 end

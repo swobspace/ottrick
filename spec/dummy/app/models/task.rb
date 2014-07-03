5a8fbfd4ff11a9278379da014e5213b19deae56e
Wolfgang Barth <wob@swobspace.net>
2014-07-03 10:11:51 +0200

    Task.to_s implemented

--- spec/dummy/app/models/task.rb
+++ spec/dummy/app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ActiveRecord::Base
   has_many :tickets, as: :ticketfor, class: Ottrick::Ticket
 
   validates :subject, :message, presence: true
+
+  def to_s
+    "(#{id}) #{subject}"
+  end
 end
